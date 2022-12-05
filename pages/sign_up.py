c8ba0e92dd3ae48b885ab0d67403d673d7e0adbc
Ignacio Reyes <ignareyesa@gmail.com>
2022-12-05 10:31:19 -0100

    Edit sign up

--- pages/sign_up.py
+++ pages/sign_up.py
@@ -1,30 +1,28 @@
-from main import logged_in, authenticator, credentials, commit_query, run_query
+from main import logged_in, authenticator, credentials, commit_query
 import streamlit as st  
 from streamlit_extras.switch_page_button import switch_page
 
-if logged_in():
-    st.warning("You are already log-in")
-    st.stop()  # App won't run anything after this line
 
+
+if logged_in():
+    st.warning("Sesión ya iniciada")
+    st.stop()  # App won't run anything after this line
 try:
-    if authenticator.register_user('Register user', preauthorization=False):
+    if authenticator.register_user('¡Únete!', preauthorization=False):
         key = list(credentials["usernames"])[-1]
         form_input = credentials["usernames"][key]
         query = f"""INSERT INTO users (email, username, name, pass) VALUES
                 {form_input["email"],key,form_input["name"], form_input["password"]}
                 """
-        results = commit_query(query)
-        st.success('User registered successfully')
-        users = run_query("SELECT * from users")
-        credentials = {"usernames": {i[2]:{"email":i[1],"name":i[3],"password":i[4]} for i in users}}
-        print(credentials)       
+        commit_query(query)
+        st.success(f'Usuario registrado correctamente')
+        st.info(f'⬇️ Ya puedes iniciar sesión')
 except Exception as e:
     if "1062 (23000)" in str(e):
-        st.error("Email already taken")
+        st.error("Correo electrónico ya en uso")
     else:
         st.error(e)
-
-signin = st.button("Already register? Sign in")
+signin = st.button("¿Ya estás registrado? Inicia sesión")
 if signin:
-    switch_page("Home")
+    switch_page("Comienza a explorar")
     
