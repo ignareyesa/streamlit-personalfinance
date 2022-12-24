6137b5910e4c20520c49bf5411e5fc51c3c38090
Ignacio Reyes <ignareyesa@gmail.com>
2022-12-24 18:35:21 -0100

    minor changes

--- init_app.py
+++ init_app.py
@@ -1,13 +1,13 @@
 # import mysql.connector
 import json
 import streamlit as st
+import os
 import yaml
 from yaml import CLoader as Loader
 import authenticator as stauth
 from dotenv import load_dotenv
 from database_connection.database import Database
-
-# from smtp_connection.email_client import EmailClient
+from smtp_connection.email_client import EmailClient
 
 load_dotenv()
 
@@ -49,18 +49,18 @@ authenticator = stauth.Authenticate(
 )
 
 
-# smtp_server = os.getenv("SMTP_SERVER")
-# smtp_port = os.getenv("SMTP_PORT")
-# smtp_username = os.getenv("SMTP_API_NAME")
-# smtp_password = os.getenv("SMTP_API_KEY")
-# smtp_from_addr = os.getenv("SMTP_FROM_ADDRESS")
-# smtp_from_name = os.getenv("SMTP_FROM_NAME")
+smtp_server = os.getenv("SMTP_SERVER")
+smtp_port = os.getenv("SMTP_PORT")
+smtp_username = os.getenv("SMTP_API_NAME")
+smtp_password = os.getenv("SMTP_API_KEY")
+smtp_from_addr = os.getenv("SMTP_FROM_ADDRESS")
+smtp_from_name = os.getenv("SMTP_FROM_NAME")
 
-# email_client = EmailClient(
-#     smtp_server=smtp_server,
-#     smtp_port=smtp_port,
-#     username=smtp_username,
-#     password=smtp_password,
-#     from_addr=smtp_from_addr,
-#     from_name=smtp_from_name,
-# )
+email_client = EmailClient(
+    smtp_server=smtp_server,
+    smtp_port=smtp_port,
+    username=smtp_username,
+    password=smtp_password,
+    from_addr=smtp_from_addr,
+    from_name=smtp_from_name,
+)
--- pages/reset_pass.py
+++ pages/reset_pass.py
@@ -1,5 +1,5 @@
-from init_db import credentials, authenticator, commit_query, run_query
-from gen_functions import logged_in, load_css_file, switch_page_button
+from init_app import credentials, authenticator, db
+from gen_functions import logged_in, load_css_file, multile_button_inline
 import streamlit as st
 
 load_css_file("styles/forms.css")
@@ -7,7 +7,7 @@ load_css_file("styles/forms.css")
 if not logged_in():
     st.warning("Para poder cambiar tu contraseña tienes que haber iniciado sesión.")
     # Show a button to go back to the login page
-    switch_page_button(["Volver a iniciar sesión"],["Comienza a explorar"])
+    multile_button_inline(["Volver a iniciar sesión"],["Comienza a explorar"])
 
     st.stop()
 
@@ -18,16 +18,16 @@ else:
         username = st.session_state["username"]
         query_id = "SELECT id from users where username=%s"
         # Get id from database
-        user_id = run_query(query_id, (username,))[0][0]
+        user_id = db.fetchone(query_id, (username,))[0]
         # If the user entered a new password, commit the change to the database
         if authenticator.reset_password(username, "Cambiar contraseña"):
             new_pass = credentials["usernames"][st.session_state["username"]][
                 "password"
             ]
             query_pass = "UPDATE users SET pass=%s WHERE id=%s"
-            commit_query(query_pass, (new_pass, user_id))
+            db.commit(query_pass, (new_pass, user_id))
             st.success("Contraseña modificada correctamente")
     except Exception as e:
         st.error(e)
     # Show a button to go back to the login page
-    switch_page_button(["Volver a iniciar sesión"],["Comienza a explorar"])
+    multile_button_inline(["Volver a iniciar sesión"],["Comienza a explorar"])
