03d30f619b4d55161cb346541570aa89db03a972
Ignacio Reyes <ignareyesa@gmail.com>
2022-12-24 18:27:58 -0100

    Able to delete several rows at a time

--- pages/consult_movements.py
+++ pages/consult_movements.py
@@ -4,12 +4,11 @@ from time import sleep
 
 # Import the necessary libraries
 from st_aggrid import AgGrid, GridOptionsBuilder, JsCode
-from init_db import run_query, commit_query, get_query_columns, authenticator
-from gen_functions import load_css_file, switch_page_button, logged_in, create_temporary_token
+from init_app import db, authenticator
+from gen_functions import load_css_file, multile_button_inline, logged_in, create_temporary_token
 
 st.experimental_set_query_params()
 css_style = "styles/buttons.css"
-
 # Load the CSS file for the page
 load_css_file("styles/add_movements.css")
 
@@ -17,7 +16,7 @@ load_css_file("styles/add_movements.css")
 if not logged_in():
     # If not, show a warning message and a button to go back to the login page
     st.warning("Para poder consultar tus movimientos tienes que haber iniciado sesión.")
-    switch_page_button(["Volver a iniciar sesión"], ["Comienza a explorar"])
+    multile_button_inline(["Volver a iniciar sesión"], ["Comienza a explorar"])
     st.stop()
 
 authenticator.logout("Cerrar sesión", "sidebar")
@@ -25,7 +24,7 @@ authenticator.logout("Cerrar sesión", "sidebar")
 # Get the user's ID from the database
 username = st.session_state["username"]
 query_id = "SELECT id from users where username=%s"
-user_id = run_query(query_id, (username,))[0][0]
+user_id = db.fetchone(query_id, (username,))[0]
 
 # Get the user's income movements from the database
 query = """SELECT "incomes" AS movement,
@@ -46,16 +45,16 @@ query = """SELECT "incomes" AS movement,
                     ORDER BY date DESC;
                 """
 
-data = run_query(query, (user_id, user_id))
-columns = get_query_columns(query, (user_id, user_id))
+data = db.fetchall(query, (user_id, user_id))
+columns = db.get_columns(query, (user_id, user_id))
 
 # Convert the data to a pandas DataFrame
 df = pd.DataFrame(data=data, columns=columns)
 
 # If the user has not added any income movements, show a warning and a button to go to the add movements page
 if len(df) == 0:
-    st.warning("No ha añadido ningún ingreso")
-    switch_page_button(["Añadir movimiento"], ["add movements"])
+    st.warning("No ha añadido ningún movimiento")
+    multile_button_inline(["Añadir movimiento"], ["Añadir movimientos"])
     st.stop()
 
 # Select the data to be displayed in the grid
@@ -87,8 +86,8 @@ function(params) {
 """
 )
 gb = GridOptionsBuilder.from_dataframe(df_selection)
-gb.configure_selection("single", use_checkbox=True, header_checkbox=True)
-gb.configure_pagination()
+gb.configure_selection("multiple", use_checkbox=True, header_checkbox=True, suppressRowClickSelection=True)
+gb.configure_pagination(paginationAutoPageSize=False, paginationPageSize=15)
 gb.configure_column("Cantidad", cellStyle=cellsytle_jscode)
 
 # Build the grid options
@@ -103,23 +102,27 @@ grid_response = AgGrid(
 )
 
 # Delete or Modify register buttons
-delete_register = st.button("Eliminar Registro")
-modify_register = st.button("Modificar registro")
+# consult_action = multile_button_inline(["Eliminar Registro","Modificar registro"], ["delete","modify"], ids_as_links=False, css=css_style)
+delete = st.button("Eliminar registro(s)","delete")
+modify = st.button("Modificar registro","modify")
 
+# st.write(consult_action)
 # If there is no register selected and click button, show a warning
 if len(grid_response["selected_rows"]) == 0:
-    if delete_register:
+    if delete or modify:
         st.warning("No ha seleccionado ningún registro, seleccione antes de eliminar")
-    if modify_register:
-        st.warning("No ha seleccionado ningún registro, seleccione antes de modificar")
-else:
+        
+    # if modify
+    #     st.warning("No ha seleccionado ningún registro, seleccione antes de modificar")
+        
+elif len(grid_response["selected_rows"]) == 1:
     # Get response from selection
     response = dict(grid_response)["selected_rows"][0]
     selected_row = int(response["_selectedRowNodeInfo"]["nodeId"])
     movement_id = int(df.iloc[selected_row]["id"])
     
     # Define variable table depending on whether it is income or expense
-    if response["Cantidad"] > 0:
+    if response["Cantidad"] >= 0:
         table_name = "incomes_movements"
         option = "Ingresos"
     elif response["Cantidad"] < 0:
@@ -127,18 +130,75 @@ else:
         option = "Gastos"
     else:
         st.error("Hay algun error")
-    if delete_register:
+    if delete:
         query = f"DELETE FROM {table_name} where id=%s"
-        commit_query(query, (movement_id,))
+        db.commit(query, (movement_id,))
         st.success(
             "Registro eliminado correctamente, la página se volvera a cargar automáticamente en un instante."
         )
         sleep(1.7)
+        consult_action = None
         st.experimental_rerun()
-    if modify_register:
+    if modify:
         token = create_temporary_token(table="modify_movement_tokens")
         nav_script = """
             <meta http-equiv="refresh" content="0; url='%s'">
             """ % (f"""/Añadir movimientos?page=modify_movement&token={token}-{movement_id}&username={username}&option={option}&placeholder={df.iloc[selected_row]["concept"]}""")
         st.write(nav_script, unsafe_allow_html=True)
 
+else:
+    response = dict(grid_response)["selected_rows"]
+    incomes = []
+    expenses = []
+    for selection in response:
+        if selection["Cantidad"] >= 0:
+            incomes.append(selection["_selectedRowNodeInfo"]["nodeId"]) 
+        else:
+            expenses.append(selection["_selectedRowNodeInfo"]["nodeId"])
+    
+    incomes_ids = tuple(df.iloc[incomes]["id"].values)
+    expenses_ids = tuple(df.iloc[expenses]["id"].values)
+    if len(incomes_ids) == 1:
+        incomes_query = f"DELETE FROM incomes_movements where id ={incomes_ids[0]}"
+    else:
+        incomes_query = f"DELETE FROM incomes_movements where id in {incomes_ids}"
+    
+    if len(expenses_ids) == 1:
+        expenses_query = f"DELETE FROM expenses_movements where id = {expenses_ids[0]}"
+    else:
+        expenses_query = f"DELETE FROM expenses_movements where id in {expenses_ids}"
+
+    if delete:
+        if incomes_ids and expenses:
+            db.commit(incomes_query)
+            db.commit(expenses_query)
+            st.success(
+            "Registros eliminados correctamente, la página se volvera a cargar automáticamente en un instante."
+            )
+            sleep(1.7)
+            consult_action = None
+            st.experimental_rerun()
+        
+        elif incomes_ids and not expenses_ids:
+            db.commit(incomes_query)
+            st.success(
+            "Registros eliminados correctamente, la página se volvera a cargar automáticamente en un instante."
+            )
+            sleep(1.7)
+            consult_action = None
+            st.experimental_rerun()
+        
+        elif expenses_ids and not incomes_ids:
+            db.commit(expenses_query)
+            st.success(
+            "Registros eliminados correctamente, la página se volvera a cargar automáticamente en un instante."
+            )
+            sleep(1.7)
+            consult_action = None
+            st.experimental_rerun()
+        else:
+            pass
+        
+
+    if modify:
+        st.warning("Solo se puede modificar en registro de una vez.")
