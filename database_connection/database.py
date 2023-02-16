70f74e4b0720fe288cc46b3aab10bb15f0c2198c
Ignacio Reyes <ignareyesa@gmail.com>
2023-02-16 05:57:05 -0100

    my sql lose connection #5

--- database_connection/database.py
+++ database_connection/database.py
@@ -58,7 +58,7 @@ class Database:
         """
         try:
             cursor = self.connection.cursor(buffered=True)
-        except (mysql.connector.errors.OperationalError, mysql.connector.errors.InternalError, mysql.connector.errors.InterfaceError):
+        except 
             self.connect()
             cursor = self.connection.cursor(buffered=True)
         cursor.execute(query, params)
@@ -80,7 +80,7 @@ class Database:
         """
         try:
             cursor = self.connection.cursor(buffered=True)
-        except (mysql.connector.errors.OperationalError, mysql.connector.errors.InternalError, mysql.connector.errors.InterfaceError):
+        except 
             self.connect()
             cursor = self.connection.cursor(buffered=True)
         cursor.execute(query, params)
@@ -103,7 +103,7 @@ class Database:
         """
         try:
             cursor = self.connection.cursor(buffered=True)
-        except (mysql.connector.errors.OperationalError, mysql.connector.errors.InternalError, mysql.connector.errors.InterfaceError):
+        except 
             self.connect()
             cursor = self.connection.cursor(buffered=True)
         cursor.execute(query, params)
@@ -126,7 +126,7 @@ class Database:
         """
         try:
             cursor = self.connection.cursor(buffered=True)
-        except (mysql.connector.errors.OperationalError, mysql.connector.errors.InternalError, mysql.connector.errors.InterfaceError):
+        except 
             self.connect()
             cursor = self.connection.cursor(buffered=True)
         cursor.execute(query, params)
