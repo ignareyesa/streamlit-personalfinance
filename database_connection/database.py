3fe61a36f6150b0562c17b2d842a067624868c5e
Ignacio Reyes <ignareyesa@gmail.com>
2023-02-21 15:48:31 -0100

    my sql lose connection #6

--- database_connection/database.py
+++ database_connection/database.py
@@ -58,7 +58,7 @@ class Database:
         """
         try:
             cursor = self.connection.cursor(buffered=True)
-        except 
+        except:
             self.connect()
             cursor = self.connection.cursor(buffered=True)
         cursor.execute(query, params)
@@ -80,7 +80,7 @@ class Database:
         """
         try:
             cursor = self.connection.cursor(buffered=True)
-        except 
+        except:
             self.connect()
             cursor = self.connection.cursor(buffered=True)
         cursor.execute(query, params)
@@ -103,7 +103,7 @@ class Database:
         """
         try:
             cursor = self.connection.cursor(buffered=True)
-        except 
+        except:
             self.connect()
             cursor = self.connection.cursor(buffered=True)
         cursor.execute(query, params)
@@ -126,7 +126,7 @@ class Database:
         """
         try:
             cursor = self.connection.cursor(buffered=True)
-        except 
+        except:
             self.connect()
             cursor = self.connection.cursor(buffered=True)
         cursor.execute(query, params)
